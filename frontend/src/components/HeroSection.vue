bd4c46ef1f07cf892ad2f73b1cb9244524315f7d
BlerianI <istrefi.b21@htlwienwest.at>
2025-06-27 19:30:50 +0200

    Searchbar + Herosection

---
+++ frontend/src/components/HeroSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <q-parallax src="../../public/bleach.avif" :speed="0.5" :height="350">
+      <q-input rounded outlined  placeholder="Suche" bg-color="white">
+        <template #append>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    </q-parallax>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/pages/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> MiList </q-toolbar-title>
+
+        <q-btn color="white" label="Login" class="text-blue-10"/>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <HeroSection></HeroSection>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import HeroSection from '../components/HeroSection.vue';
+</script>
+
+<style lang="scss" scoped></style>
